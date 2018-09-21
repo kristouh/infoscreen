bb6d14b13403321678ba86498f5cdd6411a09cd4
Kristoffer Høisæther <kris-u-h@hotmail.com>
2018-09-21 16:32:08 +0200

    skyss.sh done

--- skyss.sh
+++ skyss.sh
@@ -4,13 +4,13 @@
 #url
 straume='https://reiseplanlegger.skyss.no/scripts/TravelMagic/TravelMagicWE.dll/avgangsinfo?context=wap.xhtml&lang=no&from=Straume%20bro%20(Bergen)'
 
-tider_sentrum=()
-nummer_sentrum=()
-navn_sentrum=()
+tider_sentrum_arr=()
+nummer_sentrum_arr=()
+navn_sentrum_arr=()
 
-tider_soreide=()
-nummer_soreide=()
-navn_soreide=()
+tider_soreide_arr=()
+nummer_soreide_arr=()
+navn_soreide_arr=()
 
 sentrum='12015515'
 soreide='12015528'
@@ -29,9 +29,35 @@ navn_soreide=${navn_soreide//til /til_}
 navn_soreide=${navn_soreide// terminal/_terminal}
 navn_soreide=${navn_soreide// Lufthavn/_Lufthavn}
 
+for i in $tider_sentrum; do
+	tider_sentrum_arr+=("$i")
+done
+for i in $nummer_sentrum; do
+	nummer_sentrum_arr+=("$i")
+done
+for i in $navn_sentrum; do
+	navn_sentrum_arr+=("$i")
+done
 
+for i in $tider_soreide; do
+	tider_soreide_arr+=("$i")
+done
+for i in $nummer_soreide; do
+	nummer_soreide_arr+=("$i")
+done
+for i in $navn_soreide; do
+	navn_soreide_arr+=("$i")
+done
 
+ant_sentrum=${#tider_sentrum_arr[@]}
+ant_soreide=${#tider_soreide_arr[@]}
 
+echo 'Sentrum' > 'straumebro.txt'
+for i in $(seq 0 $ant_sentrum); do
+	echo "${nummer_sentrum_arr[$i]} ${navn_sentrum_arr[$i]} ${tider_sentrum_arr[$i]}" >> 'straumebro.txt'
+done
 
-
-
+echo 'Søreide' >> 'straumebro.txt'
+for i in $(seq 0 $ant_sentrum); do
+	echo "${nummer_soreide_arr[$i]} ${navn_soreide_arr[$i]} ${tider_soreide_arr[$i]}" >> 'straumebro.txt'
+done
